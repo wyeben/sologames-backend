7ba8d9fdfc8a8c1ea02f7bbb02431ff61426b4b0
Wyeben <yila.ben@yahoo.com>
2024-01-30 18:00:51 +0100

    added code

--- src/main/java/com/sologamesbackend/sologamesbackend/servises/UserService.java
+++ src/main/java/com/sologamesbackend/sologamesbackend/servises/UserService.java
@@ -15,6 +15,7 @@ public class UserService {
         User user = new User();
         user.setFirstName(userRequest.getFirstName());
         user.setLastName(userRequest.getLastName());
+
         user.setPassword(userRequest.getPassword());
         UserRepositoryImplementation userRepositoryImplementation = new UserRepositoryImplementation();
         userRepositoryImplementation.save(user);
